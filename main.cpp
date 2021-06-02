1a3c78b4d9c127ee6e180faf3be147f042c93716
lordjuacs <juacsr@gmail.com>
2021-06-01 20:48:53 -0500

    insert, delete, search Sequential File

--- main.cpp
+++ main.cpp
@@ -5,26 +5,21 @@
 #include "Record/IncidentRecord.h"
 
 int main() {
-
-
     SequentialFile<Team> sq("../data/Teams.csv", "Teams");
     std::vector<str> splitLine = {"Hola FC", "11", "0", "2", "2", "2", "-4", "1", "5", "-4"};
     SFRecord<Team> testTeam(splitLine);
 
-   sq.insert(testTeam);
+    sq.insert(testTeam);
 
-  auto efe = sq.load();
-  for (auto item : efe)
-      item.print();
-/*
-  std::cout << "________________________________________\n";
 
-  //auto searchResult = sq.searchByRanges("Arsenal", "Wolves");
-  //sq.deleteRecord("AFC Bournemouth");
+    std::cout << "________________________________________\n";
 
-  auto efe = sq.load();
-  for (auto item : efe)
-      item.print();
-  */
+    //auto searchResult = sq.searchByRanges("Arsenal", "Wolves");
+    sq.deleteRecord("Hola FC");
+
+    auto efe = sq.load();
+    for (auto item : efe)
+        item.print();
+
     return 0;
 }
